2eed243c07fff8cad71c5ec2a6e5d7e5e463dd31
semedi <ssemedi@opennebula.org>
2017-11-24 20:30:33 +0100

    main program almost finished

---
+++ custom_temp.c
@@ -0,0 +1,40 @@
+/**
+ * This example shows how to use the Thermo3 Click wrapper of the LetMeCreate
+ * library.
+ *
+ * It reads the temperature from the sensor and exits.
+ *
+ * The Thermo3 Click must be inserted in Mikrobus 1 before running this program.
+ */
+
+#include <stdio.h>
+#include <letmecreate/letmecreate.h>
+
+
+int main(void)
+{
+    float temperature = 0.f;
+    char command[64];
+    char ip[16];
+    int n;
+
+    printf( "IP to pub:");
+    scanf("%s", ip);
+
+    printf(command, "echo %s > hijodeputa", ip);
+
+    if (n > 0)
+        system(command);
+
+    i2c_init();
+    i2c_select_bus(MIKROBUS_1);
+
+    thermo3_click_enable(0);
+    thermo3_click_get_temperature(&temperature);
+    printf("temperature: %.3f degrees celsius\n", temperature);
+    thermo3_click_disable();
+
+    i2c_release();
+
+    return 0;
+}
